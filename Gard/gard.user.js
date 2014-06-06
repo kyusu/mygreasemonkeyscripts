f88be3dcf4b90215dcb3542d01918c8af79563fc
Christian Dittrich <christian.dittrich@seitenbau.com>
2014-06-06 16:22:46 +0200

    Added automatic reloader for GARD

---
+++ Gard/gard.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name           The Midgard automatic reloader
+// @namespace      https://github.com/kyusu/mygreasemonkeyscripts
+// @description    Adds missing features and remedies annoyances
+// @include        https://midgard.intra.t-online.de/gard/secure/Dashboard.jspa
+// @match          https://midgard.intra.t-online.de/gard/secure/Dashboard.jspa
+// ==/UserScript==
+
+(function () {
+    var hidden = "hidden";
+
+    // Standards:
+    if (hidden in document)
+        document.addEventListener("visibilitychange", onchange);
+    else if ((hidden = "mozHidden") in document)
+        document.addEventListener("mozvisibilitychange", onchange);
+    else if ((hidden = "webkitHidden") in document)
+        document.addEventListener("webkitvisibilitychange", onchange);
+    else if ((hidden = "msHidden") in document)
+        document.addEventListener("msvisibilitychange", onchange);
+    // IE 9 and lower:
+    else if ('onfocusin' in document)
+        document.onfocusin = document.onfocusout = onchange;
+    // All others:
+    else
+        window.onpageshow = window.onpagehide
+            = window.onfocus = window.onblur = onchange;
+
+    function onchange(evt) {
+        var v = 'visible', h = 'hidden',
+            result,
+            evtMap = {
+                focus: v, focusin: v, pageshow: v, blur: h, focusout: h, pagehide: h
+            };
+
+        evt = evt || window.event;
+        if (evt.type in evtMap) {
+            result = evtMap[evt.type];
+        }
+        else {
+            result = this[hidden] ? "hidden" : "visible";
+        }
+        if (result === v) {
+            window.location.reload();
+        }
+    }
+})();
