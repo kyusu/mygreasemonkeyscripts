39854a6670960438dbd6bcd21c661a3932fa6e68
Christian Dittrich <christian.dittrich@seitenbau.com>
2014-07-02 17:52:47 +0200

    Switched myproject user script hosting

--- MyProject/myproject.user.js
+++ MyProject/myproject.user.js
@@ -9,8 +9,8 @@
 // The script should only execute in the top frame, not in iFrames.
 if (top.location.href === window.location.href) {
     (function () {
-        var scriptURI = 'https://myproject.telekom.de/pi/attachments/download/22692/myproject.js',
-            cssURI = 'https://myproject.telekom.de/pi/attachments/download/22693/myproject.css',
+        var scriptURI = 'https://raw.githubusercontent.com/kyusu/mygreasemonkeyscripts/master/MyProject/myproject.js',
+            cssURI = 'https://raw.githubusercontent.com/kyusu/mygreasemonkeyscripts/master/MyProject/myproject.css',
             script = document.createElement('script'),
             link = document.createElement('link');
 
