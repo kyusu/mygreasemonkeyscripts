c98847b29693ffc3703b9046e0320889570ee4e8
kyusu <feedmewithspam@web.de>
2016-02-18 13:30:13 +0100

    Add toggle button to switch between editable and non editable view

--- MyProjectNextGeneration/myproject.nex.gen.user.js
+++ MyProjectNextGeneration/myproject.nex.gen.user.js
@@ -12,6 +12,10 @@
     var pollerId;
     var toolBarItems;
     var rightPanelHidden = false;
+    var editableDescriptionHidden = false;
+    var domId = 'user-script-non-editable-description';
+    var magenta = '#e20074';
+    var originalColor = 'rgb(248, 248, 248)';
 
     var buttonFactory = function (label, clickCallback) {
         var button = document.createElement('button');
@@ -28,8 +32,39 @@
         toolBarItems.appendChild(li);
     };
 
-    var hideRightPanelButton = buttonFactory('💩', function (e) {
+    var showNonEditableDescription = function () {
+        var workPackageDescription = document.querySelector('#work-package-description');
+        var description = workPackageDescription.querySelector('.inplace-editing--container').innerHTML;
+        var div = document.createElement('div');
+        div.id = domId;
+        div.innerHTML = description;
+        workPackageDescription.appendChild(div);
+        var editableDescription = workPackageDescription.querySelector('.inplace-edit.attribute-description');
+        editableDescription.style.display = 'none';
+    };
+
+    var hideNonEditableDescription = function () {
+        var workPackageDescription = document.querySelector('#work-package-description');
+        var editableDescription = workPackageDescription.querySelector('.inplace-edit.attribute-description');
+        editableDescription.style.display = 'block';
+        var nonEditableDescription = document.querySelector('#' + domId);
+        workPackageDescription.removeChild(nonEditableDescription)
+    };
+
+    var addNonEditableDescriptionButton = buttonFactory('X', function (e) {
         e.preventDefault();
+        e.target.style.backgroundColor = editableDescriptionHidden ? originalColor : magenta;
+        if (editableDescriptionHidden) {
+            hideNonEditableDescription();
+        } else {
+            showNonEditableDescription();
+        }
+        editableDescriptionHidden = !editableDescriptionHidden;
+    });
+
+    var hideRightPanelButton = buttonFactory('->', function (e) {
+        e.preventDefault();
+        e.target.style.backgroundColor = rightPanelHidden ? originalColor : magenta;
         var rightPanel = document.querySelector('#work-packages-index .work-packages--split-view .work-packages--right-panel');
         rightPanel.style.display = rightPanelHidden ? 'block' : 'none';
         var leftPanel = document.querySelector('#work-packages-index .work-packages--split-view .work-packages--left-panel');
@@ -43,6 +78,7 @@
             toolBarItems = document.querySelector('#toolbar-items');
             clearInterval(pollerId);
             addButton(hideRightPanelButton);
+            addButton(addNonEditableDescriptionButton);
         }
     };
     pollerId = setInterval(checkToolBarPresence, 300);
