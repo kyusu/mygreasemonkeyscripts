a226afa64355558ca69d8c94c476a60d4b35afc3
kyusu <feedmewithspam@web.de>
2017-09-11 10:21:01 +0200

    Add BitBucket "delete branch" script

---
+++ BitBucket/deleteBranchCheckAutoCheck.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name           The "Delete Branch" enable
+// @description    Enables the "Delete Branch" checkbox on the BitBucket pull request dialog for you
+// @include        https://bitbucket.kkeu.de/projects/*/repos/*/pull-requests/*
+// @run-at         document-start
+// ==/UserScript==
+
+(function () {
+    document.addEventListener('click', e => {
+        if (e.target && e.target.classList.contains('merge-button')) {
+            const deleteBranchCheckbox = document.getElementById('delete-branch-checkbox');
+            if (deleteBranchCheckbox) {
+                deleteBranchCheckbox.checked = true;
+            }
+        }
+    });
+}());
