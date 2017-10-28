fd6e4746048ae7bd6f0f855a24f30e8c736ac5ab
kyusu <feedmewithspam@web.de>
2017-10-28 13:23:47 +0200

    Add copy branch name to clipboard script

---
+++ BitBucket/copyBranchNameToClipboard.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name           Copy branch name to clipboard
+// @description    Copy the branch name of the current pull request to the clipboard so it can used on the command line
+// @include        https://bitbucket.kkeu.de/projects/*/repos/*/pull-requests/*
+// @grant          GM_setClipboard
+// @run-at         document-idle
+// ==/UserScript==
+
+(function () {
+    const branchNameEls = document.getElementsByClassName('branch-name');
+    if (branchNameEls) {
+        const branchNameEl = branchNameEls[0];
+        if (branchNameEl) {
+            branchNameEl.style.cursor = 'pointer';
+            branchNameEl.addEventListener('click', ev => {
+                const branchName = ev.target.innerText;
+                GM_setClipboard(branchName);
+            });
+        }
+    }
+}());
+
+
+
+
